3e8a96d08224bf1ea7d47d01c575c9353f9019ec
coby98765 <coby98765@gmail.com>
2025-05-12 12:18:25 +0300

    first commit

---
+++ series-analyzer/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Example
+{
+    class Programe()
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("abc");
+            //run menu
+            int user_input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(user_input);
+            switch (user_input)
+            {
+                // case 0:
+                //     {Console.WriteLine("abc");}
+
+            }
+        }
+
+        // show list
+
+        // edit list
+
+        // sorted list
+
+        // max func
+
+        // min func
+
+        // average func
+
+        // sum of list
+
+        //
+
+    }
+}
