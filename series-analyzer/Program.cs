d1353b4b99949bf4ff1831915c12b4bc555844ba
coby98765 <coby98765@gmail.com>
2025-05-13 09:36:38 +0300

    FindMax function

--- series-analyzer/Program.cs
+++ series-analyzer/Program.cs
@@ -77,24 +77,36 @@ namespace series_analyzer
         }
 
 
-            // max func
-            // return<int> max item in list
+        // max func
+        // return<int> max item in list
+        static void FindMax(List<int> seriesList)
+        {
+            int max = seriesList[0];
+            foreach (int i in seriesList)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+            Console.WriteLine($"The Maximum Number in the list: {max}");
+        }
 
 
-            // min func
-            // return<int> min item in list
+        // min func
+        // return<int> min item in list
 
-            // average func
-            // return<int> average of value of items in list
+        // average func
+        // return<int> average of value of items in list
 
-            // sum of list
-            // return<int> sum of value of items in list
+        // sum of list
+        // return<int> sum of value of items in list
 
-            // split string
-            // validate items (str,positive,at lest 3 items) and then add to "series" List 
-            // if not invalid jump to "enter list"
+        // split string
+        // validate items (str,positive,at lest 3 items) and then add to "series" List 
+        // if not invalid jump to "enter list"
 
-            List<int> SplitString(string[] series_input)
+        List<int> SplitString(string[] series_input)
         {
             List<int> ints = new List<int>();
             foreach (string num in series_input)
